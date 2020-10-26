f3f220bc5f26521b7bb0b4036d74a347fd33188b
Garrett <Garrett.Bodley@gmail.com>
2020-10-26 12:37:54 -0400

    commit tree 559bb248b41684f80a2810c9cf448054e14a2b10

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,68 @@
 
+#  takes in an array of integers and returns a copy of the array with the integers in ascending order.
+def sort_array_asc(array)
+    array.sort!
+end
+
+# Build a method sort_array_desc that takes in an array of integers and returns a copy of the array with the integers in descending order. 
+# Remember that .sort takes a block in which you can specify how you want your array sorted.
+
+def sort_array_desc (array)
+    array.sort! {|a, b| b <=> a}
+end
+
+# Build a method sort_array_char_count that takes in an array of strings and returns a copy of the array with the strings ordered in ascending order by length. 
+# Remember that .sort takes a block in which you can specify how you want your array sorted.
+
+def sort_array_char_count (array)
+    array.sort! {|a, b| a.length <=> b.length}
+end
+
+# Build a method swap_elements that takes in an array and swaps the second and third elements.
+# Remember that array indices start at 0, so the second element has an index of 1 and the third element has an index of 2.
+
+def swap_elements (array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+# Build a method reverse_array that takes in an array of integers and returns a copy of the array with the elements in reverse order.
+
+def reverse_array(array)
+    array.reverse!
+end
+
+# Build a method called kesha_maker that takes in an array of strings and replaces the third character in each string with a $ ("dollar sign")—Ke$ha style. 
+# Use the .each method to iterate and build a new array to return at the end of your method, just like we did in the "My Each" lab.
+
+def kesha_maker(array)
+    kesha = []
+    array.each do |word|
+        chars = word.split(//)
+        chars[2] = "$"
+        word = chars.join
+        kesha << word
+    end
+    kesha
+end
+
+# Build a method find_a that returns all the strings in the array passed to it that start_with? (hint) the letter "a". 
+# You'll want to use a high level iterator for this that finds, selects, or detects elements based on a condition.
+
+def find_a (array)
+    found = array.select {|word| word[0] == "a"}
+end
+
+# Build a method sum_array that adds together all of the integers in the array and returns their sum.
+
+def sum_array(array)
+    sum = 0
+    array.each {|num| sum += num}
+    sum
+end
+
+# Build a method that adds an "s" to each word in the array except for the second element in the array ("feet" is already plural).
+
+def add_s (array)
+    array.each {|word| word << "s" if word != array[1]}
+end
